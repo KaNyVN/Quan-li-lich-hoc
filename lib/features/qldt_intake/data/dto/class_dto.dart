c6a25510470e9a8066a3a51c4f219045c81f1153
Cirrus CI <support@cirruslabs.org>
2026-09-16 04:03:56 +0000

    Add Class DTO

---
+++ lib/features/qldt_intake/data/dto/class_dto.dart
@@ -0,0 +1,21 @@
+class ClassDto {
+  final String maMonHoc;
+  final String tenMonHoc;
+  final String giangVien;
+  final String phongHoc;
+  final String thuHoc;
+  final String tietHoc;
+  final String gioBatDau;
+  final String gioKetThuc;
+
+  ClassDto({
+    required this.maMonHoc,
+    required this.tenMonHoc,
+    required this.giangVien,
+    required this.phongHoc,
+    required this.thuHoc,
+    required this.tietHoc,
+    required this.gioBatDau,
+    required this.gioKetThuc,
+  });
+}
